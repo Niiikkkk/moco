dcaca333e84d7a419ba149527d6dda6149974c1d
nicholas.berardo <berardonicholas5@gmail.com>
2024-08-07 16:22:50 +0200

    sbatch

--- tools/train.py
+++ tools/train.py
@@ -24,7 +24,8 @@ paddle.disable_static()
 from passl.utils import config as cfg_util
 print("CIAO")
 sys.stdout.flush()
-from passl.engine.engine import Engine
+import passl.engine.engine as ok
+# from passl.engine.engine import Engine
 
 
 def main():
@@ -32,7 +33,7 @@ def main():
     config = cfg_util.get_config(
         args.config, overrides=args.override, show=False)
     config.profiler_options = args.profiler_options
-    engine = Engine(config, mode="train")
+    engine = ok.Engine(config, mode="train")
     engine.train()
 
 
