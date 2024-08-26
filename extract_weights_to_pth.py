1924efd910f1f57cfa6bb12e9b6f073d4dbcd4a0
nicholas.berardo <berardonicholas5@gmail.com>
2024-08-26 21:16:59 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -4,7 +4,7 @@ import paddle
 #CONVERTIRE il weights.pd senza backbone.
 
 
-model = paddle.load('PATHHHHHHHH')
+model = paddle.load('/home/nberardo/moco/output_dir/moco_v1_r50/weights.pd')
 new_state_dict = {}
 """for name,weights in model["state_dict"].items():
     if name.startswith("module.encoder."):
@@ -15,4 +15,4 @@ for x, item in model.items():
     if x.startswith("backbone."):
         x=x.replace("backbone.","")
         new_state_dict[x]=item
-paddle.save(new_state_dict, 'resnet50_extracted.pd')
+paddle.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v1_r50/resnet50_extracted.pd')
