a761c6d982ed8ff377191d14408fb3d22af34b3f
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-07 12:29:37 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -7,4 +7,7 @@ for x in model:
     print(x)
 for name,weights in model.items():
     new_state_dict[name] = weights
+print("")
+for x in new_state_dict:
+    print(x)
 #torch.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v2_r50/resnet50_extracted.pth')
