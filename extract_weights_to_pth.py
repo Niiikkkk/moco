ef5dbdc423fb52cb3dd9b7066afd203c9f9336ef
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-12 11:18:06 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -5,7 +5,7 @@ import paddle
 
 model = paddle.load('/home/nberardo/moco/output_dir/moco_v1_r50/epoch_200.pd')
 new_state_dict = {}
-for name,weights in model.items():
+for name,weights in model["state_dict"].items():
     print(name)
     #name = "backbone." + name
     #new_state_dict[name] = torch.from_numpy(weights.numpy())
