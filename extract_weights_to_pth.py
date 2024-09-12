8951fd28d7782f42a4061da2f96b04da880a2f5d
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-12 11:16:18 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -8,5 +8,5 @@ new_state_dict = {}
 for name,weights in model.items():
     print(name)
     #name = "backbone." + name
-    new_state_dict[name] = torch.from_numpy(weights.numpy())
+    #new_state_dict[name] = torch.from_numpy(weights.numpy())
 #torch.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v2_r50/resnet50_extracted.pth')
