7ac1afbd91796b9b4f60aba138afd4eafd996283
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-12 10:41:12 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -7,6 +7,5 @@ model = paddle.load('/home/nberardo/moco/output_dir/moco_v1_r50/resnet50_extract
 new_state_dict = {}
 for name,weights in model.items():
     name = "backbone." + name
-    print(name)
     new_state_dict[name] = torch.from_numpy(weights.numpy())
-#torch.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v1_r50/resnet50_extracted.pth')
+torch.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v1_r50/resnet50_extracted.pth')
