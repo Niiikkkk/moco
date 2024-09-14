4f79207087b7687ba50f31947c3dc4ec0b799eee
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-14 08:05:15 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -3,8 +3,8 @@ import paddle
 
 
 
-model = paddle.load('/home/nberardo/moco/output_dir/moco_v2_r50/moco_v2_r50_e200_ckpt.pdparams')
+model = paddle.load('/home/nberardo/moco/output_dir/moco_v2_r50/moco_v2_r50_e200_ckpt.pdparams.1')
 new_state_dict = {}
 for name,weights in model.items():
     new_state_dict[name] = torch.from_numpy(weights.numpy())
-torch.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v2_r50/moco_v2_r50_e200_ckpt.pdparams.pth')
+torch.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v2_r50/moco_v2_r50_e200_ckpt.pth')
