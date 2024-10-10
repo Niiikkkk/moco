f251c64972275e12e16acb5a3cf16a93d0840608
nicholas.berardo <berardonicholas5@gmail.com>
2024-10-10 13:37:05 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -11,6 +11,6 @@ for name,weights in model["state_dict"].items():
     #     new_state_dict[name] = torch.from_numpy(weights.numpy())
 print("")
 model = torch.load('/home/nberardo/vicreg/experiment/resnet50.pth')
-for name,weights in model["state_dict"].items():
+for name,weights in model.items():
     print(name)
 # torch.save(new_state_dict, '/home/nberardo/moco/moco_v2_r50_clas.pth')
