ff8cc6a8ac697faae4ec092b965a42244ab508ec
nicholas.berardo <berardonicholas5@gmail.com>
2024-10-10 13:46:48 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -1,7 +1,7 @@
 import torch
 import paddle
 
-model = paddle.load('/home/nberardo/moco/moco_v2_r50_clas.pdparams')
+model = paddle.load('/home/nberardo/moco/moco_v1_r50_clas.pdparams')
 new_state_dict = {}
 for name,weights in model["state_dict"].items():
     if name.startswith('backbone.'):
@@ -9,4 +9,4 @@ for name,weights in model["state_dict"].items():
         name = name.replace('_mean', 'running_mean')
         name = name.replace('_variance', 'running_var')
         new_state_dict[name] = torch.from_numpy(weights.numpy())
-torch.save(new_state_dict, '/home/nberardo/moco/moco_v2_r50_downloaded.pth')
+torch.save(new_state_dict, '/home/nberardo/moco/moco_v1_r50_downloaded.pth')
