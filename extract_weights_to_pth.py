5e0df28e6158d254370efc64e81dfa85fe03a2c2
nicholas.berardo <berardonicholas5@gmail.com>
2024-10-11 12:24:54 +0200

    sbatch

--- extract_weights_to_pth.py
+++ extract_weights_to_pth.py
@@ -10,4 +10,4 @@ for name,weights in model["state_dict"].items():
         name = name.replace('_variance', 'running_var')
         new_state_dict[name] = torch.from_numpy(weights.numpy())
         print(name)
-torch.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v1_r50/epoch_pth')
+torch.save(new_state_dict, '/home/nberardo/moco/output_dir/moco_v1_r50/resnet50.pth')
